82bc037e8fa07e5ee4a34fb0d94a4498b12e6131
Yuxuan Lin <yuxuan.l@icloud.com>
2022-08-07 01:57:22 +0800

    feat: add radio

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const anim = ref('fade')
+</script>
 
 <template>
   <div>
@@ -6,8 +10,17 @@
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </nav>
+
+    <div class="check">
+      <label> <input type="radio" name="anim" value="fade" v-model="anim" /> Fade </label>
+      <label> <input type="radio" name="anim" value="slide" v-model="anim" /> Slide </label>
+      <label> <input type="radio" name="anim" value="slide-down" v-model="anim" /> Slide down </label>
+      <label> <input type="radio" name="anim" value="grow-in" v-model="anim" /> Grow in </label>
+      <label> <input type="radio" name="anim" value="grow-out" v-model="anim" /> Grow out </label>
+    </div>
+
     <router-view v-slot="{ Component }">
-      <transition name="fade" mode="out-in">
+      <transition :name="anim" mode="out-in">
         <component :is="Component"></component>
       </transition>
     </router-view>
@@ -38,6 +51,16 @@ nav a:hover,
 nav a.router-link-exact-active {
   color: crimson;
   text-decoration: underline;
+}
+
+.check {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.check label {
+  margin: 0 0.5rem;
 }
 
 main {
