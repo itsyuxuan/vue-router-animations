1414b5b189856d0e680d1af05fdf829c7e8b661f
Yuxuan Lin <yuxuan.l@icloud.com>
2022-08-06 19:34:38 +0800

    feat: lorem

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <main>
+    <img src="https://picsum.photos/800/300" alt="placeholder">
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet assumenda, ducimus et vero aperiam reprehenderit?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi temporibus voluptas dolores fugit nisi debitis.</p>
+  </main>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <main>
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, impedit.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod provident, tenetur quidem exercitationem quo molestiae, dolorem rerum tempore, facilis beatae cum iure dolore odit saepe.</p>
+  </main>
+</template>
