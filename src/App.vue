bd4f85e792f11c8477f9765fa73edea516fe9bb9
Yuxuan Lin <yuxuan.l@icloud.com>
2022-08-06 19:34:08 +0800

    feat: add nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <div>Hello, World!</div>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view v-slot="{ Component }">
+      <transition>
+        <component :is="Component"></component>
+      </transition>
+    </router-view>
+  </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
